c707671a98cd00a51bda1e3bdef59dfb3864aaa8
diegojumagdao <diegojumagdao@gmail.com>
2026-03-11 14:51:02 +0100

    Implement UserInterface class

---
+++ part07-Part07_07.RecipeSearch/src/main/java/UserInterface.java
@@ -0,0 +1,106 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Admin
+ */
+public class UserInterface {
+
+    private Scanner scanner;
+    private ArrayList<Recipes> recipes;
+
+    public UserInterface(ArrayList<Recipes> recipes, Scanner scan) {
+        this.recipes = recipes;
+        this.scanner = scan;
+    }
+
+    public void start() {
+        System.out.println(" ");
+        System.out.println("Commands:");
+        System.out.println("list - lists the recipes");
+        System.out.println("stop - stops the program");
+        System.out.println("find name - searches recipes by name");
+        System.out.println("find cooking time - searches recipes by cooking time");
+        System.out.println("find ingredient - searches recipes by ingredient");
+
+        while (true) {
+            System.out.println(" ");
+            System.out.println("Enter command: ");
+            String command = scanner.nextLine();
+
+            if (command.equals("stop")) {
+                break;
+            }
+           commandHandle(command);
+        }
+    }
+
+    public void commandHandle(String command) {
+        if (command.equals("list")) {
+            listRecipes();
+        }
+        if (command.equals("find name")) {
+            findByName();
+        }
+        if (command.equals("find cooking time")) {
+            findCookingTime();
+        }
+        if (command.equals("find ingredient")) {
+            findIngredient();
+        }
+    }
+
+    public void listRecipes() {
+        System.out.println(" ");
+        System.out.println("Recipes: ");
+        for (Recipes recipe : this.recipes) {
+            System.out.println(recipe);
+        }
+    }
+
+    public void findByName() {
+        System.out.println("Searched word: ");
+        String search = this.scanner.nextLine();
+        for (Recipes recipe : this.recipes) {
+            if (recipe.getRecipe().contains(search)) {
+                System.out.println(" ");
+                System.out.println("Recipes: ");
+                System.out.println(recipe);
+            }
+        }
+    }
+
+    public void findCookingTime() {
+        System.out.println("Max cooking time: ");
+        int time = Integer.valueOf(this.scanner.nextLine());
+        System.out.println(" ");
+        System.out.println("Recipes: ");
+        for (Recipes recipe : this.recipes) {
+            if (recipe.getCookingTime() <= time) {
+                System.out.println(recipe);
+            } else {
+                continue;
+            }
+        }
+    }
+
+    public void findIngredient() {
+        System.out.println("Ingredient:");
+        String find = this.scanner.nextLine();
+        System.out.println(" ");
+        System.out.println("Recipes: ");
+        for (Recipes ingredient : this.recipes) {
+            if (ingredient.getIngredients().contains(find)) {
+                System.out.println(ingredient);
+            }
+        }
+    }
+
+}
