26f5b5042888f50a6feb0117c90decc4121c56fe
diegojumagdao <diegojumagdao@gmail.com>
2026-03-11 15:41:15 +0100

    Implement Add and All methods

---
+++ part07-Part07_08.BigYear/src/main/java/Bird.java
@@ -0,0 +1,34 @@
+
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Admin
+ */
+public class Bird {
+    private ArrayList<String> birds;
+    private String name;
+    private String latinName;
+    private int observations;
+    
+    public Bird(String name, String latinName) {
+        this.birds = new ArrayList<>();
+        this.name = name;
+        this.latinName = latinName;
+        this.observations = 0;
+    }
+    
+    public ArrayList<String> getBird() {
+        return this.birds;
+    }
+    
+    public String toString() {
+        return this.name + "(" + this.latinName + "): " + this.observations + " observations";
+    }
+}
--- part07-Part07_08.BigYear/src/main/java/mainProgram.java
+++ part07-Part07_08.BigYear/src/main/java/mainProgram.java
@@ -1,4 +1,5 @@
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class mainProgram {
@@ -7,8 +8,39 @@ public class mainProgram {
         // NB! Do not create other scanner objects than the one below
         // if and when you create other classes, pass the scanner to them
         // as a parameter
-
+        ArrayList<Bird> birdList = new ArrayList<>();
         Scanner scan = new Scanner(System.in);
+
+        while (true) {
+//            Implement as ui.start()
+            System.out.println("? ");
+            String input = scan.nextLine();
+            
+            if (input.equals("Quit")) {
+                break;
+            }
+            if (input.equals("Add")) {
+//                add(); IMPLEMENT METHOD
+                System.out.println("Name: ");
+                String name = scan.nextLine();
+                System.out.println("Name in Latin: ");
+                String latin = scan.nextLine();
+
+                birdList.add(new Bird(name, latin));
+            }
+            if (input.equals("Observation")) {
+//                observe(); IMPLEMENT METHOD
+            }
+            if (input.equals("All")) {
+//                printAllBirds(); IMPLEMENT METHOD
+                for (Bird birds : birdList) {
+                    System.out.println(birds);
+                }
+            }
+            if (input.equals("One")) {
+//                printOneBird(); IMPLEMENT METHOD
+            }
+        }
 
     }
 
